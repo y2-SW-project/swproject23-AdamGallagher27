530a4366af172f66634cf211a339bb15c8133399
Adam <N00211418@iadt.ie>
2023-02-14 23:47:08 +0000

    created test component

---
+++ source-code/app/Http/Livewire/CountDown.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CountDown extends Component
+{
+    public function render()
+    {
+        return view('livewire.count-down');
+    }
+}
