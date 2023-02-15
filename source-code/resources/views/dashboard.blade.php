19120a0d4f426a2c4e3bf8ddd1d5a9fa8899db7f
Adam <N00211418@iadt.ie>
2023-02-15 00:32:13 +0000

    started experimenting with countdown

--- source-code/resources/views/dashboard.blade.php
+++ source-code/resources/views/dashboard.blade.php
@@ -10,6 +10,34 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <script>
+
+                        const countDown = () => {
+                        const endTime = new Date("Jul 25, 2023 16:37:52").getTime();
+
+                        const timer = setInterval(() => {
+                        const currentTime = new Date().getTime()
+                        let timeLeft = endTime - currentTime
+
+                        
+                        // let days = Math.floor(timeleft / (1000 * 60 * 60 * 24))
+                        // let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+                        // let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60))
+                        // let seconds = Math.floor((timeleft % (1000 * 60)) / 1000)
+                        
+
+                        console.log(timeLeft)
+                        
+                        if (timeLeft <= 0) {
+                            clearInterval(timer)
+                            return 'time up'
+                        }
+                        }, 1000)
+}
+
+                        countDown()
+
+                    </script>
                     <livewire:count-down>
                 </div>
 
