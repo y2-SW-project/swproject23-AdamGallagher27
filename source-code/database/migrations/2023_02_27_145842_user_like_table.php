da83c04300553874cd995506b9e175caf917fec1
Adam <n00211418@iadt.ie>
2023-02-27 15:01:04 +0000

    create user likes table

---
+++ source-code/database/migrations/2023_02_27_145842_user_like_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_like', function (Blueprint $table) {
+            $table->id();
+
+            // fk for users
+            $table->bigInteger("user_id")->unsigned();
+            $table->foreign("user_id")->references("id")->on("users")->onUpdate("cascade")->onDelete("restrict");
+
+            // fk for guitars
+            $table->bigInteger("guitar_id")->unsigned();
+            $table->foreign("guitar_id")->references("id")->on("guitars")->onUpdate("cascade")->onDelete("restrict");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_like');
+    }
+};
