e14b29a8683044da00f6e7543de0b5b07ef3e4d4
Adam <n00211418@iadt.ie>
2023-02-27 15:01:43 +0000

    create guitars table

---
+++ source-code/database/migrations/2015_02_27_142934_guitars_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guitars', function (Blueprint $table) {
+
+            // normal columns
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('make');
+            $table->string('bid_expiration');
+            $table->float('price');
+
+            
+            // fk for types
+            $table->bigInteger("type_id")->unsigned();
+            $table->foreign("type_id")->references("id")->on("types")->onUpdate("cascade")->onDelete("restrict");
+
+            // fk for conditions
+            $table->bigInteger("condition_id")->unsigned();
+            $table->foreign("condition_id")->references("id")->on("conditions")->onUpdate("cascade")->onDelete("restrict");
+
+            // fk for users
+            $table->bigInteger("user_id")->unsigned();
+            $table->foreign("user_id")->references("id")->on("users")->onUpdate("cascade")->onDelete("restrict");
+
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guitars');
+    }
+};
