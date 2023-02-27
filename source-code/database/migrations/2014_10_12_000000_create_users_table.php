c6f658c86af655f506d23d83ca8473f6020942c9
Adam <n00211418@iadt.ie>
2023-02-27 15:01:56 +0000

    added user roles

--- source-code/database/migrations/2014_10_12_000000_create_users_table.php
+++ source-code/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+
+            $table->bigInteger("role_id")->unsigned();
+            $table->foreign("role_id")->references("id")->on("roles")->onUpdate("cascade")->onDelete("restrict");
+
+
             $table->rememberToken();
             $table->timestamps();
         });
