a050ff0b938d2e63987370df0c7a37c7514fc2ed
Adam <n00211418@iadt.ie>
2023-02-27 15:01:25 +0000

    create user bid table

---
+++ source-code/database/migrations/2023_02_27_144422_user_bid_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_bid', function (Blueprint $table) {
+            $table->id();
+
+            // bid amount
+            $table->dateTime("bid_expiration");
+
+            // fk for users
+            $table->bigInteger("user_id")->unsigned();
+            $table->foreign("user_id")->references("id")->on("users")->onUpdate("cascade")->onDelete("restrict");
+
+            // fk for guitars
+            $table->bigInteger("guitar_id")->unsigned();
+            $table->foreign("guitar_id")->references("id")->on("guitars")->onUpdate("cascade")->onDelete("restrict");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_bid');
+    }
+};
