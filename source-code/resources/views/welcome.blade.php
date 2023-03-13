fa1eedb89708a6c8cbe755d7416c2b42f2d62de5
Adam <n00211418@iadt.ie>
2023-03-13 14:27:56 +0000

    removed styles

--- source-code/resources/views/welcome.blade.php
+++ source-code/resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
     </head>
     <body>
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div>
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
