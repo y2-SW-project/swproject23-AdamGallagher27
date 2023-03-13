d554d77ed8beb64eb2c8b2aefa7c2b64eda9911b
Adam <n00211418@iadt.ie>
2023-03-13 15:15:42 +0000

    added guitar relationships

--- source-code/app/Models/Guitar.php
+++ source-code/app/Models/Guitar.php
@@ -9,6 +9,23 @@ class Guitar extends Model
 {
     use HasFactory;
 
+    // this is the relationship for guitars / likes
+    public function userLikes() {
+        return $this->hasMany(UserLike::class);
+    }
 
+    // this is the relationship for Condition and guitars (1:M)
+    public function condition()
+    {
+        return $this->belongsTo(Condition::class);
+    }
+
+    // relationship for type and guitars
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
     
+    // prevents mass assignment error
+    protected $guarded = [];
 }
