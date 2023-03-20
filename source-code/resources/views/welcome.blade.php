9db7b036f3a0d279a0219f3257047ebd9d8e996c
Adam <N00211418@iadt.ie>
2023-03-20 14:11:51 +0000

    commented login code out

--- source-code/resources/views/welcome.blade.php
+++ source-code/resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     </head>
     <body>
         <div>
-            @if (Route::has('login'))
+            {{-- @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
@@ -21,8 +21,8 @@
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
                         @endif
                     @endauth
-                </div>
-            @endif
+                </div> 
+            @endif--}}
 
             {{-- testing nav bar component --}}
             <x-navbar/>
