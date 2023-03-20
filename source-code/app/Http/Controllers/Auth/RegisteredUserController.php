95a95f2d54608b08f35a200f2eca3a0f982e4613
Adam <N00211418@iadt.ie>
2023-03-20 14:10:25 +0000

    added description and role id

--- source-code/app/Http/Controllers/Auth/RegisteredUserController.php
+++ source-code/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,12 +34,16 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'description' => ['required'],
+            'role_id' => ['required']
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'description' => $request->description,
+            'role_id' => $request->role_id,
         ]);
 
         event(new Registered($user));
