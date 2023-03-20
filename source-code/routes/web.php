c93cfb80ad465d301a1aec695006db80afb63657
Adam <N00211418@iadt.ie>
2023-03-20 14:12:52 +0000

    uncommented dashboard

--- source-code/routes/web.php
+++ source-code/routes/web.php
@@ -30,9 +30,9 @@ Route::get('/product', function () {
     return view('product');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
