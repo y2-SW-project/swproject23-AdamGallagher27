b6d346a30cdfa870e92f3711a70378ffc4af071b
Adam <N00211418@iadt.ie>
2023-03-27 11:59:09 +0100

    started user roles

--- source-code/app/Models/Role.php
+++ source-code/app/Models/Role.php
@@ -16,4 +16,22 @@ class Role extends Model
 
     // prevents mass assignment error
     protected $guarded = [];
+
+
+    public function authorizeRoles($roles) {
+        if(is_array($roles)){
+            return $this->hasAnyRole($roles) ||
+            abort(401, 'this action is unauthorized');
+        }
+        return $this->hasRole($roles) ||
+        abort(401, 'this action is unauthorized');
+    }
+
+    public function hasRole($role) {
+        return null !== $this->roles()->where('name', $role)->first();
+    }
+
+    public function hasAnyRole($roles) {
+        return null !== $this->roles()->whereIn('name', $roles)->first();
+    }
 }
