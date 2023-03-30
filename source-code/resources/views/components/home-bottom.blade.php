9a94c914902082e7692d29f8ddbd2a86150302e3
Adam <N00211418@iadt.ie>
2023-03-30 16:23:39 +0100

    styled home bottom component

--- source-code/resources/views/components/home-bottom.blade.php
+++ source-code/resources/views/components/home-bottom.blade.php
@@ -1,25 +1,28 @@
-<div>
+<div class="flex justify-center mb-5">
+    <div class="flex flex-col w-4/6">
 
-    {{-- shop now section --}}
-    <section>
-        <img src="*" alt="man playing guitar">
-
-        <div>
-            <h3>Buy Sustainably Sourced Guitars</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id lobortis enim, id finibus ipsum. Vestibulum mollis nisi nunc, sed fermentum nisl </p>
-            <button>Shop Now</button>
-        </div>
-    </section>
-
-    {{-- create shop section --}}
-    <section>
-        <div>
-            <h3>Buy Sustainably Sourced Guitars</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id lobortis enim, id finibus ipsum. Vestibulum mollis nisi nunc, sed fermentum nisl </p>
-            <button>Shop Now</button>
-        </div>
-
-        <img src="*" alt="image of a guitar shop">
-
-    </section>
+        {{-- shop now section --}}
+        <section class="flex items-center gap-20">
+            <img src="{{url('/images/guitar-solo.png')}}" alt="man playing guitar">
+    
+            <div>
+                <h3>Buy Sustainably Sourced Guitars</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id lobortis enim, id finibus ipsum. Vestibulum mollis nisi nunc, sed fermentum nisl </p>
+                <button>Shop Now</button>
+            </div>
+        </section>
+    
+        {{-- create shop section --}}
+        <section class="flex items-center gap-20">
+            <div>
+                <h3>Buy Sustainably Sourced Guitars</h3>
+                <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id lobortis enim, id finibus ipsum. Vestibulum mollis nisi nunc, sed fermentum nisl </p>
+                <button>Shop Now</button>
+            </div>
+    
+            <img src="{{url('/images/shop.png')}}" alt="image of a guitar shop">
+    
+        </section>
+    </div>
 </div>
+
