3031fbcbde5ff296587a9856b8c12a5040f7c059
Adam <N00211418@iadt.ie>
2023-04-05 11:25:30 +0100

    added switch for account view

--- source-code/resources/views/components/guitar-product.blade.php
+++ source-code/resources/views/components/guitar-product.blade.php
@@ -16,17 +16,47 @@
                     <li class="mt-3">Buy Now Price: €{{ $guitar->price }}</li>
                     <li class="mt-3">Condition: {{ $condition->condition }}</li>
                     <li class="mt-3">Type: {{ $type->type }}</li>
-                    <li class="mt-3">Posted By: {{ $user->name }}</li>
                     <li class="mt-3">Description: {{ $guitar->description }}</li>
+
 
                 </ul>
                 <ul>
-                    <li><button class="mt-4 bg-gray-700 hover:bg-white text-white hover:text-black font-bold py-1 border w-80">
-                        Make Bid
-                      </button></li>
-                      <li><button class="mt-1  hover:bg-gray-700 text-grey hover:text-white font-bold py-1 border border-black w-80">
-                        Buy Now
-                      </button></li>
+                    <li><button
+                            class="mt-4 bg-gray-700 hover:bg-white text-white hover:text-black font-bold py-1 border w-80">
+                            Make Bid
+                        </button></li>
+                    <li><button
+                            class="mt-1  hover:bg-gray-700 text-grey hover:text-white font-bold py-1 border border-black w-80">
+                            Buy Now
+                        </button></li>
+
+                    @switch(Auth::user()->role_id)
+                        @case(1)
+                            <a href="{{ route('user.account', ['user_id' => $user->id]) }}">
+                                <li class="mt-3">Posted By: {{ $user->name }}</li>
+
+                            </a>
+                        @break
+
+                        @case(2)
+                            <a href="{{ route('shop.account', ['user_id' => $user->id]) }}">
+                                <li class="mt-3">Posted By: {{ $user->name }}</li>
+
+                            </a>
+                        @break
+
+                        @case(3)
+                            <a href="{{ route('admin.account', ['user_id' => $user->id]) }}">
+                                <li class="mt-3">Posted By: {{ $user->name }}</li>
+
+                            </a>
+                        @break
+
+                        @default
+                            {{ Auth::user()->role_id }}
+                    @endswitch
+
+
                 </ul>
             </div>
             <div>
