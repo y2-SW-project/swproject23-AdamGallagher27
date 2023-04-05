7a87d6da1c3e9b4d527819f480d65be4d360085e
Adam <N00211418@iadt.ie>
2023-04-05 11:24:51 +0100

    added guitars function

--- source-code/app/Models/UserLike.php
+++ source-code/app/Models/UserLike.php
@@ -5,10 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Like extends Model
+class UserLike extends Model
 {
     use HasFactory;
 
     // prevents mass assignment error
     protected $guarded = [];
+
+    protected $table = 'user_like';
+
+    public function guitars() {
+        $this->belongsToMany("App/Models/Guitar", "user_like");
+    }
 }
