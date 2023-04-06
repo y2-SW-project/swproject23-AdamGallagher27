d4176d7974efc004379e913b43086cb6e5faee99
Adam <N00211418@iadt.ie>
2023-04-06 16:22:27 +0100

    rendering basic tabs comp

--- source-code/resources/views/user/guitar/account.blade.php
+++ source-code/resources/views/user/guitar/account.blade.php
@@ -1,31 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Account</title>
+    @livewireStyles
 </head>
+
 <body>
     <div>
-            {{ $user }}
-            {{ $guitar }}
-            {{ $liked }}
-            {{-- live wire tabs between posts / likes  --}}
-            
+        {{-- {{ $user }} --}}
+        {{-- {{ $guitar }} --}}
+        {{-- {{ $liked }} --}}
+        {{-- live wire tabs between posts / likes  --}}
 
-            {{-- testing nav bar component --}}
-            <x-navbar/>
 
-            {{-- table for user data --}}
-            {{-- this will take a user object --}}
-            {{-- <x-account-data/> --}}
+        {{-- testing nav bar component --}}
+        <x-navbar />
 
-            {{-- products component --}}
-            @for ($i = 0; $i < 10; $i++)
-                <x-product-card price=20/>
-            @endfor
-            
+        {{-- table for user data --}}
+        {{-- this will take a user object --}}
+        {{-- <x-account-data/> --}}
+
+        {{-- products component --}}
+        {{-- <div>
+            @foreach ($guitar as $current)
+                <x-product-card :guitar='$current' />
+            @endforeach
+        </div> --}}
+
+        <livewire:profile-tabs :products='$guitar' :likes='$liked' />
+
     </div>
+    @livewireScripts
 </body>
+
 </html>
