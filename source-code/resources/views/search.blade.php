01cbe8a46e41df4beb3c86ef0fa04ba76d38c192
Adam <N00211418@iadt.ie>
2023-04-07 17:30:16 +0100

    added pagination and search data

--- source-code/resources/views/search.blade.php
+++ source-code/resources/views/search.blade.php
@@ -1,42 +1,23 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Search</title>
+    <title>Document</title>
 </head>
 <body>
-    <div>
-        @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            {{-- testing nav bar component --}}
-            <x-navbar/>
-
-            {{-- testing search result --}}
-            <x-search-result numResults=200, searchKey="Fender" />
-
-            {{-- test for search controls --}}
-            {{-- not sure if this should be live Wire or Blade template --}}
-            {{-- most likely live wire --}}
-
-
-            {{-- search element component --}}
-            @for ($i = 0; $i < 5; $i++)
-                <x-search-card price=10, userName="Adam Test"  />
-            @endfor
-    </div>
+    <x-navbar />
+    {{ $phrase }}
+    <br>
+    
+    @foreach ($guitars as $guitar)
+        {{ $guitar }}
+        <br>
+    @endforeach
+    
+    {{ $guitars->appends(['phrase' => $phrase])->links() }}
 </body>
 </html>
