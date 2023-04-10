607fab09a4f553ca47ad728ea43b1044e929951b
Adam <N00211418@iadt.ie>
2023-04-10 16:37:12 +0100

    display success if in session

--- source-code/resources/views/admin/guitar/account.blade.php
+++ source-code/resources/views/admin/guitar/account.blade.php
@@ -12,6 +12,12 @@
 <body>
     <div>
         <x-navbar />
+        {{-- show success message for update / create --}}
+        @if (session('success'))
+            <div class="mb-4 px-4 py-2 bg-green-100 border border-green-200 text-green">
+                {{ session('success') }}
+            </div>
+        @endif
 
         <a href="{{ URL::to('admin/guitar/create') }}">create </a>
         <livewire:profile-tabs :products='$guitar' :likes='$liked' />
--- source-code/resources/views/shop/guitar/account.blade.php
+++ source-code/resources/views/shop/guitar/account.blade.php
@@ -12,6 +12,12 @@
 <body>
     <div>
         <x-navbar />
+        {{-- show success message for update / create --}}
+        @if (session('success'))
+            <div class="mb-4 px-4 py-2 bg-green-100 border border-green-200 text-green">
+                {{ session('success') }}
+            </div>
+        @endif
 
         <a href="{{ URL::to('shop/guitar/create') }}">create </a>
         {{-- table for user data --}}
