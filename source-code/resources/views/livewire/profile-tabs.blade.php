8141b530144eb079b981baf7231b26ea19a400c8
Adam <N00211418@iadt.ie>
2023-04-12 14:59:55 +0100

    styled / fixed routes

--- source-code/resources/views/livewire/profile-tabs.blade.php
+++ source-code/resources/views/livewire/profile-tabs.blade.php
@@ -1,11 +1,90 @@
-<div>
-    <button wire:click="$set('show', true)">All</button>
-    <button wire:click="$set('show', false)">Likes</button>
+<div class="mx-96 mt-5">
+    <button wire:click="$set('show', true)">
+        @if ($show)
+            <p class="underline font-medium">All</p>
+        @else
+            All
+        @endif
+    </button>
+    <button wire:click="$set('show', false)">
+        @if (!$show)
+            <p class="underline font-medium">Likes</p>
+        @else
+            Likes
+        @endif
+    </button>
     @if ($show)
-    all products
-    {{ $products }}
-    @else
-        liked products
-        {{ $likes }}
-    @endif
-</div>
+        <div class="grid grid-cols-6 mt-4">
+            @forelse ($products as $guitar)
+                <div>
+
+                    @switch(Auth::user()->role_id)
+                        {{-- user --}}
+                        @case(1)
+                            <a href="{{ route('user-guitar.show', $guitar->id) }} class">
+                                <x-product-card :guitar='$guitar' />
+                            </a>
+                        @break
+
+                        {{-- shop --}}
+                        @case(2)
+                            <a href="{{ route('shop-guitar.show', $guitar->id) }} class">
+                                <x-product-card :guitar='$guitar' />
+                            </a>
+                        @break
+
+                        {{-- admin --}}
+                        @case(3)
+                            <a href="{{ route('admin-guitar.show', $guitar->id) }} class">
+                                <x-product-card :guitar='$guitar' />
+                            </a>
+                        @break
+
+                        @default
+                    @endswitch
+
+
+                </div>
+                @empty
+                    <p>No Posts</p>
+                @endforelse
+            </div>
+        @else
+            <div class="grid grid-cols-6 mt-4">
+                @forelse ($likes as $guitar)
+                    <div>
+
+                        @switch(Auth::user()->role_id)
+                            {{-- user --}}
+                            @case(1)
+                                <a href="{{ route('user-guitar.show', $guitar->id) }} class">
+                                    <x-product-card :guitar='$guitar' />
+                                </a>
+                            @break
+
+                            {{-- shop --}}
+                            @case(2)
+                                <a href="{{ route('shop-guitar.show', $guitar->id) }} class">
+                                    <x-product-card :guitar='$guitar' />
+                                </a>
+                            @break
+
+                            {{-- admin --}}
+                            @case(3)
+                                <a href="{{ route('admin-guitar.show', $guitar->id) }} class">
+                                    <x-product-card :guitar='$guitar' />
+                                </a>
+                            @break
+
+                            @default
+                        @endswitch
+
+
+
+                    </div>
+                    @empty
+                        <p>No Likes Yet</p>
+                    @endforelse
+                </div>
+            @endif
+        </div>
