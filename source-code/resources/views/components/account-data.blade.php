a082846a5c8f07fc452f5cc994e2277c2d33c203
Adam <N00211418@iadt.ie>
2023-04-12 14:56:37 +0100

    created component

--- source-code/resources/views/components/account-data.blade.php
+++ source-code/resources/views/components/account-data.blade.php
@@ -1,10 +1,5 @@
-<div>
-    <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
-    <img src="*" alt="profile image">
-
-    {{-- link to create guitar --}}
-    <a href="*"></a>
-
-    {{-- description --}}
-    <p></p>
+<div class="mx-96">
+    <p class="text-xl">{{ $userData[0]->name }}</p>
+    <p>Description: {{ $userData[0]->description }}</p>
+    <br>
 </div>
