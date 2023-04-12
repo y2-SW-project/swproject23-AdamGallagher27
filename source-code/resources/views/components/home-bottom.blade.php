3bde73190fb1020f988012633ee55edbb1d4bd31
Adam <N00211418@iadt.ie>
2023-04-12 13:32:30 +0100

    styled component

--- source-code/resources/views/components/home-bottom.blade.php
+++ source-code/resources/views/components/home-bottom.blade.php
@@ -6,18 +6,18 @@
             <img src="{{url('/images/guitar-solo.png')}}" alt="man playing guitar">
     
             <div>
-                <h3>Buy Sustainably Sourced Guitars</h3>
+                <h3 class="text-xl font-medium">Buy Sustainably Sourced Guitars</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id lobortis enim, id finibus ipsum. Vestibulum mollis nisi nunc, sed fermentum nisl </p>
-                <button>Shop Now</button>
+                <button class="rounded border bg-slate-700 text-white px-3 mt-3" >Shop Now</button>
             </div>
         </section>
     
         {{-- create shop section --}}
         <section class="flex items-center gap-20">
             <div>
-                <h3>Buy Sustainably Sourced Guitars</h3>
+                <h3 class="text-xl font-medium">Sell Your Old Guitars</h3>
                 <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id lobortis enim, id finibus ipsum. Vestibulum mollis nisi nunc, sed fermentum nisl </p>
-                <button>Shop Now</button>
+                <button class="rounded border bg-slate-700 text-white px-3 mt-3">Sell Now</button>
             </div>
     
             <img src="{{url('/images/shop.png')}}" alt="image of a guitar shop">
