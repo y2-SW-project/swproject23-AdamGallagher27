4a5af7e6c15da8ebfe6e42788f30bc3364eb010d
Adam <N00211418@iadt.ie>
2023-04-13 17:06:22 +0100

    created footer

---
+++ source-code/resources/views/components/footer.blade.php
@@ -0,0 +1,57 @@
+
+
+    <div class="footer flex flex-col items-center text-white bg-zinc-800
+    pt-6 mt-6
+">
+    <div class="flex gap-48 ">
+        <ul>
+            <li class="font-medium">Shop</li>
+            <li>Gift cards</li>
+            <li>Site map</li>
+            <li>Capo blog</li>
+        </ul>
+
+        <ul>
+            <li class="font-medium">Sell</li>
+            <li>Sell on Capo</li>
+            <li>Teams</li>
+            <li>Forms</li>
+            <li>Affiliate & creators</li>
+        </ul>
+
+        <ul>
+            <li class="font-medium">About</li>
+            <li>Capo</li>
+            <li>Policies</li>
+            <li>Investors</li>
+            <li>Careers</li>
+            <li>Press</li>
+            <li>Impact</li>
+            <li>legal</li>
+        </ul>
+
+        <ul>
+            <li class="font-medium">Help</li>
+            <li>Help centre</li>
+            <li>Privacy settings</li>
+        </ul>
+    </div>
+    <div class="bg-zinc-900 py-4 mt-4 w-full">
+        <div class="flex gap-72 justify-center">
+            <ul class="flex gap-3">
+                <li>Ireland</li>
+                <li>English(UK)</li>
+                <li>Europe</li>
+            </ul>
+            <ul class="flex gap-3">
+                <li>2023 Capo</li>
+                <li>Terms of Use</li>
+                <li>Privacy</li>
+                <li>Cookies</li>
+                <li>Interest Based Ads</li>
+                <li>Cookies</li>
+                <li>Regions</li>
+            </ul>
+        </div>
+    </div>
+</div>
