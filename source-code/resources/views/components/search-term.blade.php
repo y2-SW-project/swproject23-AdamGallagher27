94ea8715344de00cc478f5b48d4b445aa27154a0
Adam <N00211418@iadt.ie>
2023-04-13 17:06:44 +0100

    created search term component

---
+++ source-code/resources/views/components/search-term.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center border-b-2 pb-10 pt-5">
+    <p><span class="font-medium">{{ $num }}</span> search results from</p>
+    <p class="text-xl text-main font-medium mt-3">"{{ $phrase }}"</p>
+</div>
