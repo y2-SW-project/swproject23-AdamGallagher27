62bf33d3bdbc72008c6e8fea000ac714112f240f
Adam <N00211418@iadt.ie>
2023-04-13 17:07:54 +0100

    added grid / phrase / switch for roles

--- source-code/resources/views/search.blade.php
+++ source-code/resources/views/search.blade.php
@@ -4,22 +4,63 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
     <x-navbar />
-    {{ $phrase }}
-    <br>
-    
-    @foreach ($guitars as $guitar)
-        {{ $guitar }}
-        <br>
-    @endforeach
+    <x-search-term :phrase='is_null($phrase) ? "" : $phrase' :num='$num' />
+
+    <div class="grid grid-cols-5 mt-4 mx-64">
+        @forelse ($guitars as $guitar)
+            <div>
+
+                @switch((Auth::check()) ? (Auth::user()->role_id) : ('no role'))
+                    {{-- user --}}
+                    @case(1)
+                        <a href="{{ route('user-guitar.show', $guitar->id) }} class">
+                            <x-product-card :guitar='$guitar' />
+                        </a>
+                    @break
+
+                    {{-- shop --}}
+                    @case(2)
+                        <a href="{{ route('shop-guitar.show', $guitar->id) }} class">
+                            <x-product-card :guitar='$guitar' />
+                        </a>
+                    @break
+
+                    {{-- admin --}}
+                    @case(3)
+                        <a href="{{ route('admin-guitar.show', $guitar->id) }} class">
+                            <x-product-card :guitar='$guitar' />
+                        </a>
+                    @break
+
+                    {{-- norole --}}
+                    @case('no role')
+                        <a href="{{ route('norole-guitar.show', $guitar->id) }} class">
+                            <x-product-card :guitar='$guitar' />
+                        </a>
+                    @break
+
+                    @default
+                @endswitch
+
+
+
+            </div>
+            @empty
+                <p>Sorry No Guitars</p>
+            @endforelse
+        </div>
+
     
     <div class="w-48 mt-4 mx-auto">
         {{ $guitars->appends(['phrase' => $phrase])->links() }}
     </div>
+
+    <x-footer />
 </body>
 </html>
