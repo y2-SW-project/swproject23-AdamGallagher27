e143ec59e81e03cb2b7cc2e9d140c8b4a325fe91
Adam <N00211418@iadt.ie>
2023-04-14 13:40:39 +0100

    styled forms

--- source-code/resources/views/admin/guitar/create-form.blade.php
+++ source-code/resources/views/admin/guitar/create-form.blade.php
@@ -14,40 +14,49 @@
 
     <x-navbar />
     <div class="">
-        <form action="{{ route('admin-guitar.store') }}" method="POST" enctype="multipart/form-data">
+        <form class="px-96" action="{{ route('admin-guitar.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
             @error('name')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="text" name="name" class="w-full " placeholder="name" value="{{ old('name', '') }}">
+            <input type="text" name="name" class="w-full mt-3" placeholder="name" value="{{ old('name', '') }}">
             @error('description')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="text" name="description" class="w-full " placeholder="description"
+            <input type="text" name="description" class="w-full mt-3" placeholder="description"
                 value="{{ old('description', '') }}">
             @error('make')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="text" name="make" class="w-full " placeholder="make" value="{{ old('make', '') }}">
+            <input type="text" name="make" class="w-full mt-3" placeholder="make" value="{{ old('make', '') }}">
             @error('bid_expiration')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="datetime-local" name="bid_expiration" class="w-full " placeholder="bid expiration time"
+            <input type="datetime-local" name="bid_expiration" class="w-full mt-3" placeholder="bid expiration time"
                 value="{{ old('bid_expiration', '') }}">
             @error('price')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="text" name="price" class="w-full " placeholder="price" value="{{ old('price', '') }}">
+            <input type="text" name="price" class="w-full mt-3" placeholder="price" value="{{ old('price', '') }}">
+
             @error('type_id')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="text" name="type_id" class="w-full " placeholder="type id"
-                value="{{ old('type_id', '') }}">
+
+            <select class="w-full mt-3" name="type_id" id="type_id">
+                @foreach ($types as $type)
+                    <option value="{{ $type->id }}">{{ $type->type }}</option>
+                @endforeach
+            </select>
+            
             @error('condition_id')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="text" name="condition_id" class="w-full " placeholder="condition id"
-                value="{{ old('condition_id', '') }}">
+            <select class="w-full mt-3" name="condition_id" id="condition_id">
+                @foreach ($conditions as $condition)
+                    <option value="{{ $condition->id }}">{{ $condition->condition }}</option>
+                @endforeach
+            </select>
             @error('image')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
@@ -55,7 +64,7 @@
             </x-file-input>
             <input type="hidden" name="user_id" class="w-full " value="{{ auth()->user()->id }}">
             <input type="hidden" name="_token" value="{{ Session::token() }}">
-            <button name="submit" type="Submit" class="mt-4">submit</button>
+            <button name="submit" type="Submit" class="mt-4 rounded border bg-main text-white px-3">submit</button>
         </form>
     </div>
     <x-footer />
--- source-code/resources/views/admin/guitar/edit-form.blade.php
+++ source-code/resources/views/admin/guitar/edit-form.blade.php
@@ -1,56 +1,80 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>edit guitar</title>
 </head>
+
 <body>
     <x-navbar />
     <div class="">
-        <form action="{{ route('admin-guitar.update', $guitar->id)}}" method="POST" enctype="multipart/form-data">
+        <form class="px-96" action="{{ route('admin-guitar.update', $guitar->id) }}" method="POST"
+            enctype="multipart/form-data">
             @csrf
             @method('put')
             @error('name')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input  type="text" name="name" class="w-full " placeholder="name" value="{{ old('name', $guitar->name) }}">
+            <input type="text" name="name" class="w-full mt-3" placeholder="name"
+                value="{{ old('name', $guitar->name) }}">
             @error('description')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="text" name="description" class="w-full " placeholder="description" value="{{ old('description', $guitar->description) }}">
+            <input type="text" name="description" class="w-full mt-3" placeholder="description"
+                value="{{ old('description', $guitar->description) }}">
             @error('make')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="text" name="make" class="w-full " placeholder="make" value="{{ old('make', $guitar->make) }}">
+            <input type="text" name="make" class="w-full mt-3" placeholder="make"
+                value="{{ old('make', $guitar->make) }}">
             @error('bid_expiration')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="datetime-local" name="bid_expiration" class="w-full " placeholder="bid expiration time" value="{{ old('bid_expiration', $guitar->bid_expiration) }}">
+            <input type="datetime-local" name="bid_expiration" class="w-full mt-3" placeholder="bid expiration time"
+                value="{{ old('bid_expiration', $guitar->bid_expiration) }}">
             @error('price')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="text" name="price" class="w-full " placeholder="price" value="{{ old('price', $guitar->price) }}">
+            <input type="text" name="price" class="w-full mt-3" placeholder="price"
+                value="{{ old('price', $guitar->price) }}">
             @error('type_id')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="text" name="type_id" class="w-full " placeholder="type id" value="{{ old('type_id', $guitar->type_id) }}">
+
+            <select class="mt-2 w-full" name="type_id" id="type_id">
+                @foreach ($types as $type)
+                    <option @if ($type->id == $guitar->type_id) {{ 'selected' }} @endif value="{{ $type->id }}">
+                        {{ $type->type }}</option>
+                @endforeach
+            </select>
+
+            
             @error('condition_id')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <input type="text" name="condition_id" class="w-full " placeholder="condition id" value="{{ old('condition_id', $guitar->condition_id) }}">
+
+            <select class="mt-2 w-full" name="condition_id" id="condition_id">
+                @foreach ($conditions as $condition)
+                    <option @if ($condition->id == $guitar->condition_id) {{ 'selected' }} @endif value="{{ $condition->id }}">
+                        {{ $condition->condition }}</option>
+                @endforeach
+            </select> 
+            
             @error('image')
                 <div class="alert alert-danger mt-2">{{ $message }}</div>
             @enderror
-            <x-file-input type="file" placeholder="Image" name="image" class="w-full mt-6" field="image"></x-file-input>
+            <x-file-input type="file" placeholder="Image" name="image" class="w-full mt-6" field="image">
+            </x-file-input>
             <input type="hidden" name="user_id" class="w-full " value="{{ auth()->user()->id }}">
-            <input type="hidden" name="_token" value="{{ Session::token() }}"> 
-            <button name="submit" type="Submit" class="mt-4">submit</button>
+            <input type="hidden" name="_token" value="{{ Session::token() }}">
+            <button name="submit" type="Submit" class="mt-4 rounded border bg-main text-white px-3">submit</button>
         </form>
     </div>
     <x-footer />
 
 </body>
+
 </html>
