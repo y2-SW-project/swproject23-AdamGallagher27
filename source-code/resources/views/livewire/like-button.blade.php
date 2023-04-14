30dadd0a74f10a6553d8b35722151c3b8918a31c
Adam <N00211418@iadt.ie>
2023-04-14 13:41:47 +0100

    styled button

--- source-code/resources/views/livewire/like-button.blade.php
+++ source-code/resources/views/livewire/like-button.blade.php
@@ -2,11 +2,11 @@
     {{-- if the post has not been liked display like button --}}
     @if (!self::hasLiked($guitar->id, $currentUser->id))
         <button wire:click="likeDislike({{ $guitar->id }}, {{ $currentUser->id }})" class="mt-3 rounded border px-4">Like
-            Button</button>
+            </button>
 
         {{-- else display dislike button --}}
     @else
         <button wire:click="likeDislike({{ $guitar->id }}, {{ $currentUser->id }})"
-            class="btn-link btn-danger mb-2 mt-3">Dislike Button</button>
+            class="mt-3 rounded border px-4 text-white bg-slate-950">Dislike</button>
     @endif
 </div>
